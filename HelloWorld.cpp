4670abf2e044f4d0e7eea4146898c1286a25a950
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2015-06-22 00:27:40 -0400

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Displays "Hello World" in C++
+ *
+ * @version June 21st, 2015
+ */
+
+#include <iosteam>
+/**
+ * @brief Calls something
+ */
+int main()//
+{
+  std::cout << "Hello World!";//Prints Hello World!
+}
